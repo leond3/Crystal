d17c4503aae4df17018e74f9f95ecf0ed3003c97
leond3 <59891470+leond3@users.noreply.github.com>
2022-06-05 23:23:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-var date1=new Date();
-var seconds=0;
-var playerX=100;
-var playerY=71;
-var playerZ=100;
-var yes = true
-register('PlayerInteract', () => {
-  if(Player.getHeldItem().getID() == 346){
-    date1=new Date(); 
-  }
-});
+register("renderWorld", onRender);
 
-register("renderWorld", myWorldRender);
-
-function updateTimer() {
-  var entities=World.getAllEntitiesOfType(Java.type("net.minecraft.entity.projectile.EntityFishHook"))
-  entities.forEach((entity) => {
-    if(entity.getEntity().field_146042_b==Player.getPlayer()){
-      console.log(entity.x);
-      playerX=entity.x;
-      playerY=entity.y+1;
-      playerZ=entity.z;
-    }
-  })
-  if(entities.length==0){
-    yes=false;
-  }
-  else{
-    yes=true;
-  }
+function onRender(partial) {
+	// Tessellator.drawString("Sexy dude", Player.getRenderX(), Player.getRenderY() + 2.2, Player.getRenderZ(), 16711680);
+	const dist = distanceTo(-52, 102, -46);
+	const scale = dist > 9 ? 1 : 10 / (dist + 1);
+	Tessellator.drawString("Sexy dude", -52, 102, -46, 16711680, true, scale);
 }
 
-function myWorldRender() {
-  updateTimer()
-  //yes is 1 or 2 after the bobber is thrown for the 1st time
-  //yes is 3 or 4 after bobber is thrown for the 2nd time
-  if(yes){  
-    Tessellator.drawString(Math.abs(new Date() - date1) / 1000, playerX, playerY, playerZ);
-  }
+function distanceTo(x, y, z) {
+	return Math.sqrt(Math.pow(x - Player.getRenderX(), 2) + Math.pow(y - Player.getRenderY(), 2) + Math.pow(z - Player.getRenderZ(), 2));
 }
+
+// function distanceTo(x, y, z, X, Y, Z) {
+// 	return Math.sqrt(Math.pow(x - X, 2) + Math.pow(y - Y, 2) + Math.pow(z - Z, 2));
+// }
