d2cb27a4d2f095a8a95e9dbd130dfae5efa0d45f
leond3 <59891470+leond3@users.noreply.github.com>
2022-06-06 02:38:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
 register("renderWorld", onRender);
 
-function onRender(partial) {
-	// Tessellator.drawString("Sexy dude", Player.getRenderX(), Player.getRenderY() + 2.2, Player.getRenderZ(), 16711680);
-	const dist = distanceTo(-52, 102, -46);
-	const scale = dist > 9 ? 1 : 10 / (dist + 1);
-	Tessellator.drawString("Sexy dude", -52, 102, -46, 16711680, true, scale);
+const waypoints = new Array();
+// waypoints.push(["Sexy dude", [-2.5, 71.8, -73.5], [255, 0, 0]]);
+// waypoints.push(["ello", [0.5, 99.8, 0.5], [0, 255, 255, 255]]);
+// waypoints.push(["hey", [20.5, 100.8]]);
+
+function onRender() {
+	if (waypoints.length == 0) return;
+
+	const playerPos = new Vector(Player.getRenderX(), Player.getRenderY() + 1.9, Player.getRenderZ());
+
+	for (const array of waypoints) {
+		renderWaypoint(typeof array[0] === 'string' ? playerPos : 'undefined', array[1] !== undefined ? new Vector(typeof array[1][0] === 'number' ? array[1][0] : 0, typeof array[1][1] === 'number' ? array[1][1] : 0, typeof array[1][2] === 'number' ? array[1][2] : 0) : playerPos, array[0], array[2] !== undefined ? (typeof array[2][3] === 'number' ? array[2][3] << 24 : 0) + (typeof array[2][0] === 'number' ? array[2][0] << 16 : 0) + (typeof array[2][1] === 'number' ? array[2][1] << 8 : 0) + (typeof array[2][2] === 'number' ? array[2][2] : 0) : 0);
+	}
+
+	function renderWaypoint(playerPos, waypointPos, text, color) {
+		const vec = waypointPos.subtract(playerPos)
+		const dist = vec.magnitude();
+	
+		const renderPos = dist > 10 ? playerPos.add(vec.normalise().multiply(10)) : waypointPos;
+	
+		Tessellator.drawString(text, renderPos.getX(), renderPos.getY(), renderPos.getZ(), color, true, dist > 9 ? 0.04 : 0.03 + 0.025 / (Math.sqrt(dist) + 1), false);
+		Tessellator.drawString(Math.round(dist) + 'm', renderPos.getX(), renderPos.getY() - 0.4, renderPos.getZ(), color, true, dist > 9 ? 0.03 : 0.0225 + 0.01875 / (Math.sqrt(dist) + 1), false);
+	}
 }
 
-function distanceTo(x, y, z) {
-	return Math.sqrt(Math.pow(x - Player.getRenderX(), 2) + Math.pow(y - Player.getRenderY(), 2) + Math.pow(z - Player.getRenderZ(), 2));
-}
-
-// function distanceTo(x, y, z, X, Y, Z) {
-// 	return Math.sqrt(Math.pow(x - X, 2) + Math.pow(y - Y, 2) + Math.pow(z - Z, 2));
-// }
+var Vector = (function () {
+	function Vector(x, y, z) {
+		this.setX(x);
+		this.setY(y);
+		this.setZ(z);
+	}
+	Vector.prototype.getX = function () {
+		return this.x;
+	}
+	Vector.prototype.setX = function (X) {
+		this.x = X;
+	}
+	Vector.prototype.getY = function () {
+		return this.y;
+	}
+	Vector.prototype.setY = function (Y) {
+		this.y = Y;
+	}
+	Vector.prototype.getZ = function () {
+		return this.z;
+	}
+	Vector.prototype.setZ = function (Z) {
+		this.z = Z;
+	}	
+	Vector.prototype.add = function (v) {
+		return new Vector(this.getX() + v.getX(), this.getY() + v.getY(), this.getZ() + v.getZ());
+	}	
+	Vector.prototype.subtract = function (v) {
+		return new Vector(this.getX() - v.getX(), this.getY() - v.getY(), this.getZ() - v.getZ());
+	}	
+	Vector.prototype.multiply = function (scalar) {
+		return new Vector(this.getX() * scalar, this.getY() * scalar, this.getZ() * scalar);
+	}	
+	Vector.prototype.divide = function (scalar) {
+		return new Vector(this.getX() / scalar, this.getY() / scalar, this.getZ() / scalar);
+	}	
+	Vector.prototype.magnitude = function () {
+		return Math.sqrt(this.getX() * this.getX() + this.getY() * this.getY() + this.getZ() * this.getZ())
+	}	
+	Vector.prototype.normalise = function () {
+		return this.divide(this.magnitude());
+	}
+	return Vector;
+}());
