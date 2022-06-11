01fb61b41bb5e9b2ede092176fbc1fd8829f4286
leond3 <59891470+leond3@users.noreply.github.com>
2022-06-11 14:18:37 +0200

    Added TAB support

--- Utilities/location.js
+++ Utilities/location.js
@@ -1,15 +1,23 @@
 const locations = ['crystal nucleus', 'dragon lair', 'fairy grotto', 'goblin holdout', 'goblin queen', 'khazad dum', 'lost precursor city', 'magma fields', 'mines of divan', 'mithril deposits', 'precursor remnants'];
 
+// Gets the lobby from the scoreboard.
+// If the scoreboard is malformed get the lobby from TAB.
 export function getLobby() {
     const title = ChatLib.removeFormatting(Scoreboard.getScoreboardTitle()).replace(/[^A-Z]/g, "");
     if (title.startsWith('SKYBLOCK')) {
         const text = ChatLib.removeFormatting(Scoreboard.getLineByIndex(Scoreboard.getLines().length - 1).getName());
         const lobby = text.trim().split(' ').pop().replace(/[^A-z0-9]/g, "");
         if (lobby.toLowerCase().startsWith('m')) return lobby;
+        // Scoreboard is malformed, search for lobby number in TAB
+        for (let name of TabList.getNames()) {
+            let line = ChatLib.removeFormatting(name).trim();
+            if (line.startsWith("Server:")) return line.split(' ').pop().replace('mini', 'm').replace('mega', 'M');
+        }
     }
     return '';
 }
 
+// Only returns the location if it matches a location in 'locations' (= the constant defined at the top)
 export function getCHLocation() {
     const location = getLocation();
     const loc = location.toLowerCase().replace("'s", "").replace("'", "");
@@ -20,11 +28,17 @@ export function getCHLocation() {
     return '';
 }
 
+// Gets the skyblock location of the player from the scoreboard
 function getLocation() {
     const title = ChatLib.removeFormatting(Scoreboard.getScoreboardTitle()).replace(/[^A-Z]/g, "");
     if (title.startsWith('SKYBLOCK')) {
-        const text = ChatLib.removeFormatting(Scoreboard.getLineByIndex(Scoreboard.getLines().length - 5).getName());
-        return stripSpecial(text).replace(/[-_]/g, ' ').replace(/[^A-z ']/g, '').trim();
+        const text = ChatLib.removeFormatting(Scoreboard.getLineByIndex(Scoreboard.getLines().length - 5).getName()).trim();
+        if (text.startsWith('⏣')) return stripSpecial(text).replace(/[-_]/g, ' ').replace(/[^A-z ']/g, '').trim();
+        // Unusual scoreboard format, search all existing lines
+        for (let lines of Scoreboard.getLines()) {
+            let line = ChatLib.removeFormatting(lines.getLine()).trim();
+            if (line.startsWith('⏣')) return stripSpecial(text).replace(/[-_]/g, ' ').replace(/[^A-z ']/g, '').trim();
+        }
     }
     return 'None';
 }
